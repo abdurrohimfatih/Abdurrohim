2de4100e494c0eae63262c89eeb4c03d34fa469e
abdurrohimfatih <rohimohim7@gmail.com>
2021-11-17 11:34:38 +0700

    PC-Campus

--- src/chapter5/tesdikampus/MaxMethod20Numbers.java
+++ src/chapter5/tesdikampus/MaxMethod20Numbers.java
@@ -7,14 +7,14 @@ public class MaxMethod20Numbers {
         Scanner input = new Scanner(System.in);
         int result = 0, number;
 
-        for (int i = 1; i <= 20; i++) {
+        for (int i = 1; i <= 5; i++) {
             System.out.print("Masukkan bilangan ke-" + i + ": ");
             number = input.nextInt();
 
             result = max(number, result);
         }
 
-        System.out.println(result);
+        System.out.println("\nThe maximum is " + result);
     }
 
     public static int max(int number, int result) {

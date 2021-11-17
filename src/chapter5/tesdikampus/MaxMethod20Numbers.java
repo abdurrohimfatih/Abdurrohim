36c93e0632a42d9ee4e39e16a4994162675a2c5d
abdurrohimfatih <rohimohim7@gmail.com>
2021-11-17 13:06:59 +0700

    PC-Campus

--- src/chapter5/tesdikampus/MaxMethod20Numbers.java
+++ src/chapter5/tesdikampus/MaxMethod20Numbers.java
@@ -5,11 +5,11 @@ import java.util.Scanner;
 public class MaxMethod20Numbers {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int result = 0, number;
+        int result = 0;
 
         for (int i = 1; i <= 20; i++) {
             System.out.print("Masukkan bilangan ke-" + i + ": ");
-            number = input.nextInt();
+            int number = input.nextInt();
 
             result = max(number, result);
         }
@@ -18,12 +18,11 @@ public class MaxMethod20Numbers {
     }
 
     public static int max(int number, int result) {
-        int max = result;
-
-        if (number > max) {
-            max = number;
+        if (number > result) {
+            return result = number;
         }
-
-        return max;
+        else {
+            return result;
+        }
     }
 }
--- src/chapter5/tugas/Soal522.java
+++ src/chapter5/tugas/Soal522.java
@@ -1,7 +1,20 @@
 package chapter5.tugas;
 
+import java.util.Scanner;
+
 public class Soal522 {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Loan Amount: ");
+        double loanAmount = input.nextDouble();
+
+        System.out.print("Number of Years: ");
+        int numberOfYears = input.nextInt();
+
+        System.out.print("Annual Interest Rate: ");
+        int annualInterestRate = input.nextInt();
+
 
     }
 }

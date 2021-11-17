be35dd239590fefe5e49fd59014638118adb3fd0
abdurrohimfatih <rohimohim7@gmail.com>
2021-11-17 11:32:09 +0700

    PC-Campus

---
+++ src/chapter5/tesdikampus/MaxMethod20Numbers.java
@@ -0,0 +1,29 @@
+package chapter5.tesdikampus;
+
+import java.util.Scanner;
+
+public class MaxMethod20Numbers {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int result = 0, number;
+
+        for (int i = 1; i <= 20; i++) {
+            System.out.print("Masukkan bilangan ke-" + i + ": ");
+            number = input.nextInt();
+
+            result = max(number, result);
+        }
+
+        System.out.println(result);
+    }
+
+    public static int max(int number, int result) {
+        int max = result;
+
+        if (number > max) {
+            max = number;
+        }
+
+        return max;
+    }
+}

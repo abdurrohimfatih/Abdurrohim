ea17323b1b5dc92608462ad51ed2e10b831f497f
abdurrohimfatih <rohimohim7@gmail.com>
2021-12-08 15:17:39 +0700

    PC-Campus

--- src/tugasakhir/exercise6/PentagonalNumbers.java
+++ src/tugasakhir/exercise6/PentagonalNumbers.java
@@ -6,7 +6,7 @@ public class PentagonalNumbers {
         final int NUMBER_PER_LINE = 10;
 
         for (int i = 1; i <= NUMBER_OF_PENTAGONALS; i++) {
-            System.out.printf("%7d", getPentagonalNumber(i));
+            System.out.printf("%7d", getHexagonalNumber(i));
 
 //            Jika sudah ada 10 angka, buat baris baru
             if (i % NUMBER_PER_LINE == 0)
@@ -16,5 +16,9 @@ public class PentagonalNumbers {
 
     public static int getPentagonalNumber(int n) {
         return (n * (3 * n - 1)) / 2;
+    }
+
+    public static int getHexagonalNumber(int n) {
+        return (2 * n * (2 * n - 1)) / 2;
     }
 }
